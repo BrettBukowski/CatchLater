c7160db2def17f7312e3c5e778fa84fb23f049b3
Brett Bukowski <brett.bukowski@gmail.com>
2012-02-20 14:37:43 -0700

    Add friendlier dates w/ time_ago_in_words

--- app/helpers/videos_helper.rb
+++ app/helpers/videos_helper.rb
@@ -2,4 +2,12 @@ module VideosHelper
   def window_open(body, url, attrs = {})
     link_to body, url, attrs.merge({onclick: "window.open('#{url}'); return false;"})
   end
+  
+  def created_date(date)
+    if (date < 5.days.ago)
+      date.strftime("%b %e")
+    else
+      time_ago_in_words(date) + ' ago'
+    end
+  end
 end
